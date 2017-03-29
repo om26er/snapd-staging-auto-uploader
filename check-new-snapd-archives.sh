f48704db7a62b34c563bc277a5dadc0a0819ef1e
Omer Akram <om26er@gmail.com>
2017-03-29 14:38:51 +0500

    Update to use bot user; other cleanups

--- check-new-snapd-archives.sh
+++ check-new-snapd-archives.sh
@@ -6,31 +6,11 @@ ARCHIVES='xenial-proposed'
 PACKAGE='snapd'
 OUR_LP_ID='canonical-platform-qa'
 OUR_PPA='snapd-candidate'
-DISTRO='zesty'
+DISTRO='xenial'
 ARCH='amd64'
-
-finish() {
-    rm -rf "$tempdir"
-}
 
 get_package_version_from_ppa() {
     echo $(wget -q -O - http://ppa.launchpad.net/$1/$2/ubuntu/dists/$3/main/binary-$4/Packages.gz | zcat | grep-dctrl -PX $5 -s Version | awk '{print $NF}')
-}
-
-download_source_and_upload_to_ppa() {
-    tempdir=$(mktemp -d)
-    trap finish EXIT
-    cd $tempdir
-    dget -xu https://launchpad.net/ubuntu/+archive/primary/+files/${PACKAGE}_$version_in_proposed.dsc
-    cd snapd-*/
-    sed -i 's/.*\(DEB_BUILD_OPTIONS=\).*/DEB_BUILD_OPTIONS += nocheck testkeys/' debian/rules
-    export DEBFULLNAME='Omer Akram'
-    export DEBEMAIL='omer.akram@canonical.com'
-    dch -ltestkeys "Build with testkeys."
-    dch -r 'Release with test keys.' --distribution $DISTRO
-    debuild -S -sd -k376A1FA7
-    cd -
-    dput ppa:$OUR_LP_ID/$OUR_PPA snapd_*testkey*_source.changes
 }
 
 version_in_proposed=$(rmadison snapd -s $ARCHIVES | awk '{split($0,a,"|"); print a[2]}' | sed -e 's/ //g')
@@ -43,7 +23,7 @@ is_new_version_available=$(echo $version_in_proposed $version_in_our_ppa | pytho
 
 if [ "$is_new_version_available" -eq "0" ]; then
     echo "Update available."
-    #download_source_and_upload_to_ppa
+    ./upload-to-ppa.sh https://launchpad.net/ubuntu/+archive/primary/+files/${PACKAGE}_$version_in_proposed.dsc $DISTRO $OUR_LP_ID $OUR_PPA
 else
     echo "Our ppa is already upto date."
 fi
--- upload-to-ppa.sh
+++ upload-to-ppa.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ $# -ne 4 ]; then
+    echo "Wrong number of arguments!"
+    echo "Must run as $0 <download_url> <distro> <launchpad_id> <ppa_name>"
+    exit 1
+fi
+
 DOWNLOAD_URL=$1
 DISTRO=$2
 LP_ID=$3
@@ -16,10 +22,10 @@ cd $tempdir
 dget -xu $DOWNLOAD_URL
 cd snapd-*/
 sed -i 's/.*\(DEB_BUILD_OPTIONS=\).*/DEB_BUILD_OPTIONS += nocheck testkeys/' debian/rules
-export DEBFULLNAME='Omer Akram'
-export DEBEMAIL='omer.akram@canonical.com'
+export DEBFULLNAME='platform-qa-bot'
+export DEBEMAIL='platform-qa-bot@canonical.com'
 dch -ltestkeys "Build with testkeys."
-dch -r 'Release with test keys.' --distribution $DISTRO
-debuild -S -sd -k376A1FA7
+dch -r 'Release with testkeys.' --distribution $DISTRO
+debuild -S -sd -k46C33555
 cd -
 #dput ppa:$LP_ID/$PPA snapd_*testkey*_source.changes
